64a96a07f6d6c928a6fda26590a36d5203743f26
Itfly <pla1988@gmail.com>
2018-07-29 23:25:03 +0800

    ADD: Find Peak Element

---
+++ Find Peak Element.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int FindPeakElement(int[] nums) {
+        if (nums == null || nums.Length == 0) {
+            return -1;
+        }
+        
+        var left = 0;
+        var right = nums.Length - 1;
+        while (left <= right) {
+            var mid = left + (right - left) / 2;
+            if ((mid == 0 || nums[mid] >= nums[mid - 1]) &&
+                (mid == nums.Length - 1 || nums[mid] >= nums[mid + 1])) {
+                return mid;
+            } else if (mid > 0 &&  nums[mid - 1] > nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
