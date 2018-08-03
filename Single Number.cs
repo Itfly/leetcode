3a82d7e1315983d0a263abf3b6fb8a6891d08bfe
Itfly <pla1988@gmail.com>
2018-08-03 14:56:57 +0800

    Create Single Number.cs

---
+++ Single Number.cs
@@ -0,0 +1,9 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        var single = 0;
+        foreach (var num in nums) {
+            single ^= num;
+        }
+        return single;
+    }
+}
