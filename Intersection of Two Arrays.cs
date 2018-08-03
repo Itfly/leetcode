95da64cb5cd9e0a7f114547a43944dac6ce03c22
Itfly <pla1988@gmail.com>
2018-08-03 15:02:40 +0800

    Update Intersection of Two Arrays.cs

--- Intersection of Two Arrays.cs
+++ Intersection of Two Arrays.cs
@@ -15,4 +15,21 @@ public class Solution {
         
         return result.ToArray();
     }
+    
+    // HashSet
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        var set1 = new HashSet<int>();
+        foreach (var num in nums1) {
+            set1.Add(num);
+        }
+        
+        var set2 = new HashSet<int>();
+        foreach (var num in nums2) {
+            if (set1.Contains(num)) {
+                set2.Add(num);
+            }   
+        }
+        
+        return set2.ToArray();
+    }
 }
