6a0e12c2a06f4720f551692887942c95a4cd30a3
Itfly <pla1988@gmail.com>
2018-08-08 22:12:44 +0800

    Create Insert into a Binary Search Tree.cs

---
+++ Insert into a Binary Search Tree.cs
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode InsertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        
+        TreeNode pre = null;
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.val == val) {
+                break;
+            } else if (cur.val > val) {
+                pre = cur;
+                cur = cur.left;
+            } else {
+                pre = cur;
+                cur = cur.right;
+            }
+        }
+        if (pre.val > val) {
+            pre.left = new TreeNode(val);
+        } else {
+            pre.right = new TreeNode(val);
+        }
+        
+        return root;
+    }
+}
