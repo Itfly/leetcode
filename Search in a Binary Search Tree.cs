81ba165cb92f1cfff14a387d2d95e8028cc9744a
Itfly <pla1988@gmail.com>
2018-08-08 22:01:40 +0800

    Create Search in a Binary Search Tree.cs

---
+++ Search in a Binary Search Tree.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode SearchBST(TreeNode root, int val) {
+        while (root != null) {
+            if (root.val == val) {
+                return root;
+            } else if (root.val > val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        
+        return null;
+    }
+}
