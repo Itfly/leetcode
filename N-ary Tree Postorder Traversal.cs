3f2d6a81e4d359e6a99402d058f256ddb723c92c
Itfly <pla1988@gmail.com>
2018-08-09 20:11:10 +0800

    Create N-ary Tree Postorder Traversal.cs

---
+++ N-ary Tree Postorder Traversal.cs
@@ -0,0 +1,28 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public IList<Node> children;
+
+    public Node(){}
+    public Node(int _val,IList<Node> _children) {
+        val = _val;
+        children = _children;
+}
+*/
+public class Solution {
+    public IList<int> Postorder(Node root) {
+        var sequence = new List<int>();
+        if (root == null) {
+            return sequence;
+        }
+        
+        
+        foreach (var child in root.children) {
+            sequence.AddRange(Postorder(child));
+        }
+        sequence.Add(root.val);
+        
+        return sequence;
+    }
+}
