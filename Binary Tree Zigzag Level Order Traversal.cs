9af10108e7d99d8becca3bb3ebc7bbe6c4aa2760
Itfly <pla1988@gmail.com>
2018-08-10 23:57:30 +0800

    Create Binary Tree Zigzag Level Order Traversal.cs

---
+++ Binary Tree Zigzag Level Order Traversal.cs
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> ZigzagLevelOrder(TreeNode root) {
+        var result = new List<IList<int>>();
+        if (root == null) {
+            return result;
+        }
+        
+        var queue = new Queue<TreeNode>();
+        var reversed = false;
+        queue.Enqueue(root);
+        while (queue.Count > 0) {
+            var size = queue.Count;
+            var values = new List<int>();
+            for (var i = 0; i < size; i++) {
+                var cur = queue.Dequeue();
+                values.Add(cur.val);
+                if (cur.left != null) {
+                    queue.Enqueue(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.Enqueue(cur.right);
+                }
+            }
+            if (reversed) {
+                values.Reverse();
+                reversed = false;
+            } else {
+                reversed = true;
+            }
+            result.Add(values);
+        }
+        
+        return result;
+    }
+}
