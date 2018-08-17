bac98e1060090371b9f82ab9ad831228a6d77971
Itfly <pla1988@gmail.com>
2018-08-17 14:59:29 +0800

    Update Palindrome Partitioning.cs

--- Palindrome Partitioning.cs
+++ Palindrome Partitioning.cs
@@ -1,7 +1,13 @@
 public class Solution {
     public IList<IList<string>> Partition(string s) {
         var palindromics = CalculatePalindromic(s);
-        return Partition(s, 0, palindromics);
+        //return Partition(s, 0, palindromics);
+        
+        // use backtracking
+        var results = new List<IList<string>>();
+        var result = new List<string>();
+        BackTrack(s, 0, palindromics, results, result);
+        return results;
     }
     
     private bool[,] CalculatePalindromic(string s) {
@@ -18,6 +24,23 @@ public class Solution {
         }
         
         return palindromics;
+    }
+    
+    private void BackTrack(string s, int i, bool[,] palindromics, IList<IList<string>> results, IList<string> result) {
+        if (i >= s.Length && result.Count > 0) {
+            results.Add(new List<string>(result));
+        }
+        
+        for (var j = i; j < s.Length; j++) {
+            if (!palindromics[i,j]) {
+                continue;
+            }
+            
+            var sub = s.Substring(i, j - i + 1);
+            result.Add(sub);
+            BackTrack(s, j + 1, palindromics, results, result);
+            result.RemoveAt(result.Count - 1);
+        }
     }
     
     private IList<IList<string>> Partition(string s, int i, bool[,] palindromics) {
