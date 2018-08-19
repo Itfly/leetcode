8db55c28d976ac23095c48d2b2bfdd3792398571
Itfly <pla1988@gmail.com>
2018-08-19 16:57:43 +0800

    Create Course Schedule II.cs

---
+++ Course Schedule II.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public int[] FindOrder(int numCourses, int[,] prerequisites) {
+        if (prerequisites == null || prerequisites.Length == 0) {
+            return Enumerable.Range(0, numCourses).ToArray();;
+        } 
+        
+        var degrees = new int[numCourses];
+        for(var i = 0; i < prerequisites.GetLength(0); i++) {
+            degrees[prerequisites[i, 0]]++;
+        }
+        
+        var queue = new Queue<int>();
+        for (var i = 0; i < numCourses; i++) {
+            if (degrees[i] == 0) {
+                queue.Enqueue(i);
+            }
+        }
+        
+        var count = queue.Count;
+        var result = new int[numCourses];
+        var k = 0;
+        while (queue.Count > 0) {
+            var cur = queue.Dequeue();
+            result[k++] = cur;
+            for (var i = 0; i < prerequisites.GetLength(0); i++) {
+                if (prerequisites[i, 1] == cur) {
+                    degrees[prerequisites[i, 0]]--;
+                    if (degrees[prerequisites[i, 0]] == 0) {
+                        count++;
+                        queue.Enqueue(prerequisites[i, 0]);
+                    }
+                }
+            }
+        }
+        
+        return count == numCourses ? result : new int[0];
+    }
+}
