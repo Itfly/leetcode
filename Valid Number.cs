b35899299966da1a287cc8bc75da80c12e31f66f
Itfly <pla1988@gmail.com>
2018-08-24 14:26:22 +0800

    Create Valid Number.cs

---
+++ Valid Number.cs
@@ -0,0 +1,45 @@
+public class Solution {
+    public bool IsNumber(string s) {
+        if (string.IsNullOrEmpty(s)) {
+            return false;
+        }
+        
+        var start = 0;
+        var end = s.Length;
+        while (start < end && s[start] == ' ') {
+            start++;
+        }
+        if (start < end && (s[start] == '+' || s[start] == '-')) {
+            start++;
+        }
+        bool isNum = false;
+        while (start < end && Char.IsDigit(s[start])) {
+            start++;
+            isNum = true;
+        }
+        if (start < end && s[start] == '.') {
+            start++;
+            while (start < end && Char.IsDigit(s[start])) {
+                start++;
+                isNum = true;
+            }
+        }
+        if (isNum && start < end && s[start] == 'e') {
+            start++;
+            isNum = false;
+            if (start < end && (s[start] == '+' || s[start] == '-')) {
+                start++;
+            }
+            while (start < end && Char.IsDigit(s[start])) {
+                start++;
+                isNum = true;
+            }
+        }
+        
+        while (start < end && s[start] == ' ') {
+            start++;
+        }
+        
+        return isNum && start == end;
+    }
+}
