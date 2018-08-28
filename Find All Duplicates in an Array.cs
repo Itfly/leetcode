6d219718eae2dc47d4c687046765009ed9d9f14e
Itfly <pla1988@gmail.com>
2018-08-28 17:05:46 +0800

    Create Find All Duplicates in an Array.cs

---
+++ Find All Duplicates in an Array.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public IList<int> FindDuplicates(int[] nums) {
+        var i = 0;
+        while (i < nums.Length) {
+            if (nums[i] != nums[nums[i] - 1]) {
+                var temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+        }
+        
+        var result = new List<int>();
+        for (i = 0; i < nums.Length; i++) {
+            if (nums[i] != i + 1) {
+                result.Add(nums[i]);
+            }
+        }
+        
+        return result;
+    }
+}
