392fec2f7df91618d19892318bb25017d6326699
Itfly <pla1988@gmail.com>
2018-08-29 13:51:26 +0800

    Create Most Common Word.cs

---
+++ Most Common Word.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public string MostCommonWord(string paragraph, string[] banned) {
+        var map = new Dictionary<string, int>();
+        var bannedSet = new HashSet<string>(banned);
+        var nonLetters = new HashSet<char> {' ', '!', '?', '\'', ',', ';', '.'}; 
+        
+        var sb = new StringBuilder();
+        var i = 0;
+        var max = 0;
+        while (i <= paragraph.Length) {
+            if (i == paragraph.Length || nonLetters.Contains(paragraph[i])) {
+                if (sb.Length > 0) {
+                    var word = sb.ToString().ToLower();
+                    if (!bannedSet.Contains(word)) {
+                        if (map.ContainsKey(word)) {
+                            map[word]++;
+                        } else {
+                            map[word] = 1;
+                        }
+                        max = Math.Max(max, map[word]);
+                    }
+                    sb = new StringBuilder();
+                }
+            } else {
+                sb.Append(paragraph[i]);
+            }
+            i++;
+        }
+        
+        return map.FirstOrDefault(x => x.Value == max).Key;
+    }
+}
