b62cdbc5521042210e69dd0420d2b9e03fc8199b
Itfly <pla1988@gmail.com>
2018-08-29 23:41:12 +0800

    Create Perfect Number.cs

---
+++ Perfect Number.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool CheckPerfectNumber(int num) {
+        if (num == 1) {
+            return false;
+        }
+        
+        var sum = 1;
+        for (var i = 2; i <= Math.Sqrt(num); i++) {
+            if (num % i == 0) {
+                sum += i + num / i;
+            }
+        }
+        
+        return sum == num;
+    }
+}
