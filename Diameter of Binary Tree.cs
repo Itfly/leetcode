1aebfbb38a25d2eb2005bfad16b2a528cdd8d5ef
Itfly <pla1988@gmail.com>
2018-09-03 21:12:17 +0800

    Update Diameter of Binary Tree.cs

--- Diameter of Binary Tree.cs
+++ Diameter of Binary Tree.cs
@@ -10,7 +10,7 @@
 public class Solution {
     public int DiameterOfBinaryTree(TreeNode root) {
         var (diameter, maxLen) = DiameterAndMaxLength(root);
-        return diameter == 0 ? 0 : diameter - 1;
+        return diameter;
     }
     
     private (int, int) DiameterAndMaxLength(TreeNode root) {
@@ -21,7 +21,7 @@ public class Solution {
         var (diameterL, maxLenL) = DiameterAndMaxLength(root.left);
         var (diameterR, maxLenR) = DiameterAndMaxLength(root.right);
         var maxLen = Math.Max(maxLenL, maxLenR) + 1;
-        var diameter = Math.Max(Math.Max(diameterL, diameterR), maxLenL + maxLenR + 1);
+        var diameter = Math.Max(Math.Max(diameterL, diameterR), maxLenL + maxLenR);
         
         return (diameter, maxLen);
     }
