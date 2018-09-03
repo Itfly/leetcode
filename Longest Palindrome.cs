b0dded9b4e40a1846a5c1f251e1447955b760a21
Itfly <pla1988@gmail.com>
2018-09-03 12:55:36 +0800

    Create Longest Palindrome.cs

---
+++ Longest Palindrome.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int LongestPalindrome(string s) {
+        if (string.IsNullOrEmpty(s)) {
+            return 0;
+        }
+        
+        var map = new Dictionary<char, int>();
+        foreach (var ch in s) {
+            if (map.ContainsKey(ch)) {
+                map[ch]++;
+            } else {
+                map[ch] = 1;
+            }
+        }
+        
+        var hasSingle = false;
+        var result = 0;
+        foreach (var val in map.Values) {
+            if (val % 2 == 0) {
+                result += val;
+            } else {
+                result += val / 2 * 2;
+                hasSingle = true;
+            }
+        }
+        
+        return hasSingle ? result + 1 : result;
+    }
+}
