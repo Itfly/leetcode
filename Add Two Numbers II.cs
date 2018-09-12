26ae5037ca05315e55752a1de8cc8f043ea40917
Itfly <pla1988@gmail.com>
2018-09-13 00:27:43 +0800

    Create Add Two Numbers II.cs

---
+++ Add Two Numbers II.cs
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        var n1 = GetLength(l1);
+        var n2 = GetLength(l2);
+        
+        var head = new ListNode(1);
+        head.next = (n1 > n2 ? AddTwoNumbers(l1, l2, n1 - n2) : AddTwoNumbers(l2, l1, n2 - n1));
+        
+        if (head.next.val > 9) {
+            head.next.val %= 10;
+            return head;
+        } 
+        
+        return head.next;
+    }
+    
+    private int GetLength(ListNode l) {
+        var cnt = 0;
+        while (l != null) {
+            l = l.next;
+            cnt++;
+        }
+        
+        return cnt;
+    }
+    
+    private ListNode AddTwoNumbers(ListNode l1, ListNode l2, int offset) {
+        if (l1 == null) {
+            return null;
+        }
+        
+        var current = (offset == 0 ? new ListNode(l1.val + l2.val) : new ListNode(l1.val));
+        var next = (offset == 0 ? AddTwoNumbers(l1.next, l2.next, 0) : AddTwoNumbers(l1.next, l2, offset - 1));
+        if (next != null && next.val > 9) {
+            next.val %= 10;
+            current.val++;
+        }
+        current.next = next;
+        
+        return current;
+    }
+}
